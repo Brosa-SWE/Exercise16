c2cfd177ee86537ac57a0738a6ec502137d06ae0
Brosa-SWE <per.gref@gmail.com>
2021-09-30 16:43:06 +0200

    Add project files.

---
+++ Lms.Core/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Entities
+{
+    public class Course
+    {
+
+        public int id { get; set; }
+        public string Title { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public ICollection<Module> Modules { get; set; }
+    }
+}
